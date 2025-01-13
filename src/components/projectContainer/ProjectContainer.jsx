49eb29c40f1b5fa022e0c2a47b9d175bc992cc33
inesuribeb <inesuribeb@gmail.com>
2025-01-13 19:57:08 +0100

    navbar, projectContainer, allProfiles, home, projectPage and main

--- src/components/projectContainer/ProjectContainer.jsx
+++ src/components/projectContainer/ProjectContainer.jsx
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom';
+
 import './ProjectContainer.css'
 
-function ProjectContainer () {
+function ProjectContainer({ img, user_name, id, publication_date, link }) {
     return (
-        <div></div>
+        <div className='project-container-dsk'>
+            <div className='image-container'>
+                <Link to={`/webproject/${id}`} className="image-link">
+                    <img
+                        src={img}
+                        alt={`Project by ${user_name}`}
+                    />
+                </Link>
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                    <button></button>
+                </a>
+
+            </div>
+            <div className='project-info-dsk'>
+                <p>{user_name}</p>
+                <p>{publication_date}</p>
+            </div>
+        </div>
     )
 }
 
