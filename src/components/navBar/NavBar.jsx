de8dc42f4b207cf49ded81d9650ae3739307a52e
inesuribeb <inesuribeb@gmail.com>
2025-01-16 21:47:03 +0100

    me he vuelto loca con la pijada del nav

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { NavLink, Link, useLocation, useNavigate } from 'react-router-dom';
-import SearchFilter from '../searchFilter/SearchFilter';
 import { useFilters } from '../../context/FilterProvider';
 import './NavBar.css';
 
@@ -11,6 +10,9 @@ function NavBar() {
     const { setSelectedFilters } = useFilters();
     const [isVisible, setIsVisible] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
+    const [isCompact, setIsCompact] = useState(false);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+
 
     const resetFilters = () => {
         setSelectedFilters({
@@ -30,6 +32,7 @@ function NavBar() {
         navigate('/auth?mode=register');
     };
 
+
     useEffect(() => {
         const controlNavbar = () => {
             const currentScrollY = window.scrollY;
@@ -38,6 +41,12 @@ function NavBar() {
                 setIsVisible(false);
             } else { 
                 setIsVisible(true);
+            }
+    
+            if (currentScrollY > 100) {  
+                setIsCompact(true);
+            } else {
+                setIsCompact(false);
             }
             
             setLastScrollY(currentScrollY);
@@ -50,17 +59,26 @@ function NavBar() {
         };
     }, [lastScrollY]);
 
+
+
     return (
-            <div className={`nav-bar-dsk ${isVisible ? 'nav-visible' : 'nav-hidden'}`}>
+        // <div className={`nav-bar-dsk ${isVisible ? 'nav-visible' : 'nav-hidden'}`}>
+        // <div className={`nav-bar-dsk ${isVisible ? 'nav-visible' : 'nav-hidden'} ${isCompact ? 'nav-compact' : ''}`}>
+        <div className={`nav-bar-dsk 
+            ${isVisible ? 'nav-visible' : 'nav-hidden'}
+            ${isCompact ? 'nav-compact' : ''}
+            ${isTransitioning ? 'transitioning' : ''}
+        `}>
 
+            {/* <div className='nav-content-wrapper'> */}
+            <div className={`nav-content-wrapper ${isCompact ? 'nav-content-compact' : ''}`}>
 
-            <div className='nav-content-wrapper'>
                 <div className='logo-section'>
                     <Link onClick={resetFilters} to="/">
                         <img src="/images/59C5B3B8-3718-42B1-AFAD-F82AF582B5BF.PNG" alt="Logo" />
                     </Link>
                 </div>
-{/* 
+                {/* 
                 <div className='title-section'>
                     <Link onClick={resetFilters} to="/" className="title-link">
                         <h1>Kazoku</h1>
