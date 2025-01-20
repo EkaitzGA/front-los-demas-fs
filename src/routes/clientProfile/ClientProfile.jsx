def58b808cdc86b02ee94430e81c78f134e91d7d
inesuribeb <inesuribeb@gmail.com>
2025-01-20 15:11:06 +0100

    carousel and clientProfile

--- src/routes/clientProfile/ClientProfile.jsx
+++ src/routes/clientProfile/ClientProfile.jsx
@@ -5,49 +5,74 @@ import UserInfoContainer from '../../components/userInfoContainer/UserInfoContai
 import ProjectsGridContainer from '../../components/projectsGridContainer/ProjectsGridContainer';
 import { getUserById } from '../../utils/api/fetch';
 
-const UserHeader = ({ username }) => (
+const UserHeader = ({ userData }) => (
     <section id="heading" className="section-heading">
         <div className="breadcrumb">
             <span>HOME</span>
             <span className="separator">·</span>
             <span>DIRECTORY</span>
         </div>
-        <h1>{username || 'User not found'}</h1>
+        <h1>
+            {userData?.username
+                ? `${userData.username} / ${userData.name} ${userData.lastname}`
+                : 'Usuario no encontrado'
+            }
+        </h1>  
+        <h4>{userData.specialization} </h4>  
     </section>
 );
 
 const ClientProfile = () => {
     const { id } = useParams();
-    const [username, setUsername] = useState('');
+    const [userData, setUserData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const fetchUsername = async () => {
+        const fetchUserData = async () => {
             try {
                 const response = await getUserById(id);
+                console.log('Respuesta de la API:', response);
+
                 if (response.success) {
-                    setUsername(response.data.username);
+                    setUserData(response.data);
                 }
             } catch (error) {
-                console.error('Error fetching username:', error);
+                console.error('Error fetching user data:', error);
             } finally {
                 setIsLoading(false);
             }
         };
 
         if (id) {
-            fetchUsername();
+            fetchUserData();
         }
     }, [id]);
+
+    const getSpecializationClass = (specialization) => {
+        switch (specialization) {
+            case 'UX/UI':
+                return 'ux-ui';
+            case 'Frontend':
+                return 'frontend';
+            case 'Backend':
+                return 'backend';
+            case 'Fullstack':
+                return 'fullstack';
+            default:
+                return 'none';
+        }
+    };
 
     if (isLoading) {
         return <div className="client-profile-container">Loading...</div>;
     }
 
     return (
-        <div className="client-profile-container">
-            <UserHeader username={username} />
-            <UserInfoContainer userId={id} />
+        // <div className="client-profile-container">
+        <div className={`client-profile-container ${getSpecializationClass(userData?.specialization)}`}>
+
+            <UserHeader userData={userData} />
+            <UserInfoContainer userData={userData} />
             <ProjectsGridContainer userId={id} />
         </div>
     );
